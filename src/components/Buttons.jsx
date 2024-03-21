c7a0fc244a815e0032f45594de40b36afde46eb1
Manuel Ardon <maardon09@gmail.com>
2024-03-21 01:26:10 -0400

    first commit

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,20 @@
+import "./styling.css"
+import satData from "./satData";
+
+const Buttons = ({ filterByType, setSat, displaySats }) => {
+  return (
+    <div>
+      {displaySats.map((sat, id) => {
+  return (
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
+  );
+})}
+      
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
+  );
+};
+
+export default Buttons;
